c6957eaa7e4de0c009334fbd24bf1a4ca390fe4e
Cássio Rubens <cassiorubensadv@gmail.com>
2021-06-22 14:25:56 -0300

    FEAT: Second Stage Project - hooks - contexts - sass - authentication - firebase - pages home and newroom

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,24 +1,11 @@
-import { useState, useEffect } from "react";
+import { ButtonHTMLAttributes } from "react"
 
-type ButtonProps = {
-  children?: string;
-}
+import '../styles/button.scss';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
 export function Button(props: ButtonProps) {
-  const [counter, setCounter] = useState(0)
-
-  useEffect(() => {
-    console.log(counter);
-  }, [counter])
-
-  function increment() {
-    setCounter(oldCounter => oldCounter + 1); // 1
-    console.log(counter) // 0
-  }
-
   return (
-    <button onClick={increment}>
-      {counter}
-    </button>
+    <button className="button" {...props} />
   )
 }
